475c08be68a680403595f53e289922cd67d8554c
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 20:43:22 -0500

    Memoria y tipos de datos

---
+++ tipos-datos.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 10 << endl; //entero
+    cout << 3.1416<< endl; //flotante
+    cout << true<< endl;
+    cout << 'k'<< endl;
+    cout << "Hola guys"<< endl;
+    return 0;
+}
