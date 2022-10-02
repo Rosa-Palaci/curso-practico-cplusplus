cde3e472984fd9f45e4e764067aba8b4b9f580c8
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 22:23:54 -0500

    jugando con variables

---
+++ cumple.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //librería para los comandos
+using namespace std; // para usar cout
+
+int main(){
+    int Month = 12;
+    int Day = 4;
+    Day = Month;
+    cout << Month <<endl;    
+    cout << Day <<endl;
+
+    return 0;
+}
