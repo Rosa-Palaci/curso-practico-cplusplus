09a9cc256abdc03d50fe66d382ee7444d94aa5f6
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 20:11:41 -0500

    Librería STD y ASCII art dragon

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello World!"<<endl;
+    cout<<"             _)               (_"<<endl; //imprime
+    cout<<"            _) \ /\%/\ /\_/\ / (_"<<endl; 
+    cout<<"          _)  \ \(0 0) (0 0)//  (_"<<endl;
+    cout<<"Art by     )_ -- \(oo) (oo)/ -- _( "<<endl;
+    cout<<" VampLadee )_ / /\ \__,__//\ \ _("<<endl;
+    cout<<"             )_ /   --;--   \ _("<<endl;
+    cout<<"         *.    ( (  )) ((  ) )    .* "<<endl;
+    cout<<"           '...(____)z z(____)...'"<<endl;
+    
     return 0;
 }
