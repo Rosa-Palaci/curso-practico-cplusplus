9194bb275c57c5b2fedc1cfaeb72103256a11902
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-01 22:44:22 -0500

    usando operadores para manipular variables

---
+++ operadores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int valorA = 2;
+    int valorB = 2;
+    cout << valorA + valorB <<endl;
+    valorA = valorB - 100;
+    cout << valorA << endl;
+    return 0;
+}
