58de1ae2d3d7cfb0cf202eb68ca07f072e5937f6
Rosa Palaci <palacibelrose431@gmail.com>
2022-10-02 20:40:55 -0500

    Fin de la clase de apuntadores

--- apuntadores.cpp
+++ apuntadores.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main (){
     int direccion = 420;
+    int segundaDireccion = 1234;
     int* apuntadorADireccion;
-    apuntadorADireccion = &direccion; //d ala direccion real de la variable
+    apuntadorADireccion = &direccion; //da la direccion real de la variable
+    *apuntadorADireccion = 3;
 
-    cout<<apuntadorADireccion<<endl;
+    apuntadorADireccion = &segundaDireccion; 
+
+    cout<<*apuntadorADireccion<<endl;
 
     return 0;
 }
